2f865813f392eca1004dc75048513329f0580c0c
lhamon31 <lehamon@ncsu.edu>
2021-09-15 14:09:57 -0400

    edited code

--- code/extract.temp.NC.R
+++ code/extract.temp.NC.R
@@ -21,11 +21,6 @@ r <- c(10, 16, 17)
 triangle <- county[r,]
 
 #check map projections
-#state 
-NC_geog <- spTransform(NC, crs(jan90, asText=FALSE)) 
-plot(jan90)
-plot(NC_geog, add=T)
-proj4string(NC_geog)
 #triangle
 triangle_geog <- spTransform(triangle, crs(jan90, asText=FALSE)) 
 plot(jan90)
@@ -33,20 +28,15 @@ plot(triangle_geog, add=T)
 proj4string(triangle_geog)
 
 #crop data and plot
-#state
-jan90NC = crop(jan90, NC_geog)
-plot(jan90NC)
-plot(NC_geog,add=T)
 #triangle
 jan90triangle = crop(jan90, triangle_geog)
 plot(jan90triangle)
 plot(triangle_geog, add=T)
 
 ######################################################################################
-#Jump here if you want to use a static window for calculating temperature
-#(specifically, March-June)
+#2) Extract temperature using a static 4-month window (March-June)
 
-#for adjusted months use 
+#set years 
 years = 1990:2016
 
 #for loop to read in all the files
@@ -56,12 +46,13 @@ output = data.frame(county = character(),
 
 #for loop extracts average temp from March-June
 for(y in years){
-  filenames.march<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "03", "_bil.bil", sep="")
-  filenames.april<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "04", "_bil.bil", sep="")
-  filenames.may<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "05", "_bil.bil", sep="")
-  filenames.june<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "06", "_bil.bil", sep="")
+  "C:/Users/lhamo/Documents/git/nc-butterfly-phenology/data/temp data/1990/"
+  filenames.march<- paste("C:/Users/lhamo/Documents/git/nc-butterfly-phenology/data/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "03", "_bil.bil", sep="")
+  filenames.april<- paste("C:/Users/lhamo/Documents/git/nc-butterfly-phenology/data/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "04", "_bil.bil", sep="")
+  filenames.may<- paste("C:/Users/lhamo/Documents/git/nc-butterfly-phenology/data/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "05", "_bil.bil", sep="")
+  filenames.june<- paste("C:/Users/lhamo/Documents/git/nc-butterfly-phenology/data/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "06", "_bil.bil", sep="")
   filenames1 = c(filenames.march, filenames.april, filenames.may, filenames.june)
-  filenames <- filenames1[ !grepl("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "_bil.bil", filenames1) ]
+  filenames <- filenames1[ !grepl("C:/Users/lhamo/Documentsgit/nc-butterfly-phenology/data/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, "_bil.bil", filenames1) ]
   temp_allmonths<-stack(filenames)
   tmean = calc(temp_allmonths, mean)
   tempmean = extract(tmean, triangle_geog, fun=mean)
